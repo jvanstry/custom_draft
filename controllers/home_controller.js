16fc1419e2cd4d50b200df36b7884fe9d725aba7
Jerry VanStrydonck <jvanstry@gmail.com>
2014-04-30 23:38:27 -0400

    started some acceptance tests

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,14 @@
-var _ = require('lodash-node');
 var orm = require('orm');
 
 module.exports = {
   get: function(req, res, next){
-
+    res.status(200);
+    res.set('Content-Type', 'text/html');
+    res.render('home.ejs', { title: 'home' }, function(err, html){
+      if(err)
+        console.log('/ Errored with: ', err);
+      res.send(html);
+    });
   },
   signIn: function(req, res, next){
 
