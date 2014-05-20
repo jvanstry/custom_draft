54d7b82b65a700f719c6960604017924937b8baf
Jerry VanStrydonck <jvanstry@gmail.com>
2014-05-19 21:57:28 -0400

    fixed bug for testing

--- app.js
+++ app.js
@@ -2,25 +2,20 @@ var app = require('express')();
 var routes = require('./config/routes');
 var environment = require('./config/environment');
 var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-io.sockets.on('connection', function (socket) {
-  socket.on('message', function(data){
-    socket.emit('message', data)
-  });
-  socket.on('pick-made', function (data) {
-  });
-});
+// var sockets = require('./sockets.js');
 
 
 environment(app);
 routes(app);
+// sockets(server);
+
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.send(500);
 });
 
 app.start = function(){
-  server.listen(app.get('port'), function(){
+  return server.listen(app.get('port'), function(){
     console.log("Express on port: " + app.get('port'));
   });
 };
